253705cb87b2eb8ec4b3d14e806aeed7af3955fb
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-08-16 05:42:39 -0300

    feat: Finish SmartWalletWithGuardiansAndProposeNewOwner

--- smart-wallet/script/SmartWalletWithGuardiansAndProposeNewOwner.s.sol
+++ smart-wallet/script/SmartWalletWithGuardiansAndProposeNewOwner.s.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 
-import {SmartWalletWithGuardians} from "../src/SmartWalletWithGuardians.sol";
+import {SmartWalletWithGuardiansAndProposeNewOwner} from "../src/SmartWalletWithGuardiansAndProposeNewOwner.sol";
 
 contract ScriptSmartWalletWithGuardians is Script {
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-        new SmartWalletWithGuardians();
+        new SmartWalletWithGuardiansAndProposeNewOwner();
         vm.stopBroadcast();
     }
 }
---
+++ smart-wallet/src/SmartWalletWithGuardiansAndProposeNewOwner.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SmartWalletWithGuardiansAndProposeNewOwner {
+    address payable owner;
+    address nextOwner;
+
+    uint guardianResetCount;
+    uint public confirmationsFromGuardiansForReset = 3;
+
+    mapping(address => bool) isAllowedToSend;
+    mapping(address => uint) allowance;
+
+    mapping(address => bool) guardians;
+    mapping(address => mapping(address => bool)) newxtGuardianVoteBool;
+
+    constructor(){
+        owner = payable(address(this));
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function setGuardians(address _guardian, bool _isGuardian) public{
+        require(msg.sender == owner, "You are not the owner, aborting");
+
+        guardians[_guardian] = _isGuardian;
+    }
+
+    function proposeNewOwner(address payable _newOwner) public{
+        require(guardians[msg.sender], "You are not guardian of this wallet, aborting");
+        require(newxtGuardianVoteBool[_newOwner][msg.sender] == false, "You already voted, aborting");
+
+        if(_newOwner != nextOwner){
+            nextOwner = _newOwner;
+            guardianResetCount = 0;
+        }
+
+        guardianResetCount++;
+
+        if(guardianResetCount >= confirmationsFromGuardiansForReset){
+            owner = _newOwner;
+            nextOwner = payable(address(0));
+        }
+    }
+
+    function setAllowance(address _for, uint _amount) public {
+        require(msg.sender == owner, "You are not the owner, aborting");
+        allowance[_for] = _amount;
+
+        if(_amount > 0){
+            isAllowedToSend[_for] = true;
+        }else{
+            isAllowedToSend[_for] = false;
+        }
+    }
+
+    function transfer(address payable _to, uint _amount, bytes memory payload) payable public returns(bytes memory) {
+        if(msg.sender != owner){
+            require(isAllowedToSend[_to], "Your are not allowed to send anything from this smart contract, aborting");
+            require(allowance[_to] >= _amount, "You are trying to send more than you are allowed to, aborting");
+
+            allowance[_to] -= _amount;
+        }
+
+        (bool success, bytes memory resultData) = _to.call{value: _amount}(payload);
+
+        require(success, "Aborting, call was not successful");
+
+        return resultData;
+
+    }
+}
