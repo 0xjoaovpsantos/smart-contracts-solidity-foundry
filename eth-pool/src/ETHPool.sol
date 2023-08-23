35bc5b8868a1259ad03be03b95e033bf404cb5dd
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-08-22 21:37:04 -0300

    feature: Development in progress ETHPool.sol

--- eth-pool/src/ETHPool.sol
+++ eth-pool/src/ETHPool.sol
@@ -2,5 +2,24 @@
 pragma solidity ^0.8.13;
 
 contract ETHPool {
- 
+
+    address owner;
+    uint totalStaked;
+    uint totalRewards;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getContractBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    function stake() public payable {
+        totalStaked += msg.value;
+    }
+
+    function depositRewards() public payable {
+        totalRewards += msg.value;
+    }
 }
