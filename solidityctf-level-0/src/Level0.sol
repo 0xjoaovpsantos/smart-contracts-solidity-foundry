fae861279855a104b46f2c63e848f726a8be1105
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-10-16 14:12:52 -0300

    feat: Exercise level 0 solididty ctf

---
+++ solidityctf-level-0/src/Level0.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+/////////////////// ☆☆ extropyio ☆☆ /////////////////////
+//     -漫~*'¨¯¨'*·舞~ solidity ctf ~舞·*'¨¯¨'*~漫-     //
+/////////////////////////////////////////////////////////
+
+/*
+    interface Isolution {
+        function solution() external pure returns (uint8);
+    }
+*/
+
+contract Level0 {
+
+	  function solution() external pure returns (uint8){
+        return 255;
+      }
+}
---
+++ solidityctf-level-0/test/Level0.t.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import {Test, console2} from "forge-std/Test.sol";
