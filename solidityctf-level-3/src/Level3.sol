4a20b8f86e20a1779d76662f37eccf8bac2d3585
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-10-17 12:39:55 -0300

    feat: Level 3 solidity ctf

--- solidityctf-level-3/src/Level3.sol
+++ solidityctf-level-3/src/Level3.sol
@@ -4,26 +4,11 @@ pragma solidity ^0.8.13;
 import "forge-std/console.sol";
 
 contract Level3 {
-
-
     function solution(bytes memory packed) external returns (uint16 a, bool b, bytes6 c) {
-
-        bytes1 bytesOfBool;
-        bytes1 byteFalse = 0x00;
-
         assembly {
-             a := mload(add(packed, 0x02))
-             bytesOfBool := mload(add(packed, 0x22))
-             c := mload(add(packed, 0x23))
+            a := mload(add(packed, 0x02))
+            b := mload(add(packed, 0x22))
+            c := mload(add(packed, 0x23))
         }
-
-        assembly {
-            b := true 
-
-            if eq(bytesOfBool, byteFalse){
-                b := false
-            }
-        }
-
     }
 }
