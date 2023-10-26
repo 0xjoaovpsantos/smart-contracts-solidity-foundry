1f43bc6471bd9ec34f5e407b1145a2251b663287
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-10-26 20:57:09 -0300

    refactor: Level 5 Solidity CTF in development

--- solidityctf-level-5/src/Level5.sol
+++ solidityctf-level-5/src/Level5.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/Test.sol";
+
 contract Level5 {
+    
     function solution(int256 a, int256 b) external pure returns (int256) {
-        int256 sum = a + b;
 
-        if (sum % 2 != 0) {
-            sum = sum + 1;
-        }
+        // int256 sum;
 
-        int256 response = sum / 2;
-        return response;
+        // assembly {
+        //     sum := add(a, b)
+        // }
+
+        // int256 divisao = sum / 2;
+        // int256 modulo = sum % 2;
+
+        // assembly {
+        //     let result := add(divisao, modulo)
+
+        //     mstore(0x0, result)
+        //     return (0x0, 32)
+        // }
     }
 }
--- solidityctf-level-5/test/Level5.t.sol
+++ solidityctf-level-5/test/Level5.t.sol
@@ -15,15 +15,15 @@ contract level5Test is Test {
         int256 response1 = level5.solution(5, 2);
         assertEq(response1, 4);
 
-        int256 response2 = level5.solution(4, 3);
-        assertEq(response2, 4);
 
-        int256 response3 = level5.solution(4, 4);
-        assertEq(response3, 4);
+        int256 response2 = level5.solution(10, 30);
+        assertEq(response2, 20);
+
+        int256 response3 = level5.solution(13, 2);
+        assertEq(response3, 8);
+
+         int256 response4 = level5.solution(-5, 2);
+        assertEq(response4, -2);
     }
 
-    // function testSetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
 }
