edcb9bf849a4d498921ab46023dda0c410357ed4
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-10-26 17:56:34 -0300

    refactor: Level 4 Solidity CTF

--- solidityctf-level-4/src/Level4.sol
+++ solidityctf-level-4/src/Level4.sol
@@ -4,10 +4,18 @@ pragma solidity ^0.8.13;
 contract Level4 {
     function solution(uint256 number) external pure returns (uint256) {
 
+           uint256 result;
+
         for(uint256 index = 0; index <= number; index++){
 
-            if( 2 ** (index + 1) > number){
-                return 2 ** index;
+            if(index == 0){
+                result = 1;
+            } else {
+                result = 2 * result;            
+            }
+
+            if(result + result > number){
+                return result;
             }
         }
     }
