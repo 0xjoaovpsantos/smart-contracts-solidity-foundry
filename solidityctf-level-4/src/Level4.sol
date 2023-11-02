6e4aba309d7fbbf65cb62a6d62f11933d449e651
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2023-11-02 15:28:42 -0300

    feat: Level 4 Solidity CTF

--- solidityctf-level-4/src/Level4.sol
+++ solidityctf-level-4/src/Level4.sol
@@ -5,22 +5,55 @@ contract Level4 {
     function solution(uint256 number) external pure returns (uint256) {
 
            uint256 result;
+           uint256 inputNumber = number;
+            uint256 test = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
-        for(uint256 index = 0; index <= number; index++){
+           if(number == 1){
+            return 1;
+           }
 
-            if(index == 0){
-                assembly {
+        assembly {
+            for{let index := 0} lt(index, test) {index := add(index, 1)}{
+                if eq(index, 0){
                     result := 1
                 }
-            } else {
-                assembly {
-                    result := add(result, result)
-                }          
-            }
 
-            if(result > number) {
-                return result / 2;
+                if gt(index, 0){
+                    result := add(result, result)
+                }
+
+                if gt(result, inputNumber){
+                    mstore(0x0, div(result, 2))
+                    return(0x0, 32)
+                }
             }
         }
+
+        // for(uint256 index = 0; index <= number; index++){
+
+        //     if(index == 0){
+        //         assembly {
+        //             result := 1
+        //         }
+        //     } else {
+        //         assembly {
+        //             result := add(result, result)
+        //         }          
+        //     }
+
+        //     if(result > number) {
+        //         return result / 2;
+        //     }
+
+        //     assembly {
+        //         if gt(result, inputNumber){
+        //             mstore(0x0, div(result, 2))
+        //             return(0x0, 32)
+        //         }
+        //     }
+        // }
+
+
+
     }
 }
